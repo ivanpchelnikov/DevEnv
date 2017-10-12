c6f2ef6737c4582d4c46088c58fdbe0649e06066
Ivan Pchelnikov <ivan.pchelnikov@gmail.com>
2017-10-12 20:00:47 +1100

    Added Json server for mock api calls

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import './index.css';
-import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('0,0.00');
-console.log(`I would pay ${courseValue} for this course!`); // eslint-disable-line no-console
+import {getUsers, deleteUser} from './api/userApi';
+
+getUsers().then(result => {
+
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const elemet = event.target;
+      event.preventDefault();
+      deleteUser(elemet.attributes["data-id"].value);
+      const row = elemet.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+
+    };
+  });
+});
